8b536dc238b1187d25a043eaa247f390d02ee8dc
Kunal Gupta <kunalgupta@Kunals-MacBook-Pro.local>
2015-10-12 10:49:35 -0500

    Created Autonomous file

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/AutonomousOption1.java
@@ -0,0 +1,43 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.Code;
+
+import com.qualcomm.robotcore.hardware.*;
+
+/**
+ * Created by kunalgupta on 10/12/15.
+ */
+public class AutonomousOption1 {
+
+    public DcMotor motorFR;
+    public DcMotor motorFL;
+    public DcMotor motorBR;
+    public DcMotor motorBL;
+
+    public DcMotor motorHangR;
+    public DcMotor motorHangL;
+
+    public DcMotor motorSpinner;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
