19186f7f2087589e3c3e96dff8f0f4fa7be14e66
rocketrice <provincerice@gmail.com>
2015-10-27 17:28:36 -0500

    New Algorithim

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/VariableSpeed.java
@@ -0,0 +1,20 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.Code;
+
+import static java.lang.Math.abs;
+
+/**
+ * Created by User on 10/27/2015.
+ */
+public class VariableSpeed {
+    public double newValue(double num){
+        double ans = 0;
+        if (abs(num) > .5){
+            //exponential growth
+        }
+        else{
+            //linear growth
+        }
+        return ans;
+    }
+
+}
