2b4d3c10b5a543f237f0a2f86775b1b91e084348
stev3nlo <stev3nlo17@gmail.com>
2015-11-03 13:59:28 -0600

    created autonomous for scrimmage; will simply drive to floor goal and push the debris out of the way of the mountain

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/ScrimmageAutoBlue.java
@@ -0,0 +1,25 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.Code;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Steven on 11/2/2015.
+ */
+public class ScrimmageAutoBlue extends LinearOpMode {
+    public DcMotor motorFR;
+    public DcMotor motorFL;
+    public DcMotor motorBR;
+    public DcMotor motorBL;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+
+        waitForStart();
+
+        
+    }
+}
