4e19087ce795a495bcf236be0aa6648176763d00
stev3nlo <stev3nlo17@gmail.com>
2015-11-03 17:25:22 -0600

    set up encoder variables

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/ScrimmageAutoBlue.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/ScrimmageAutoBlue.java
@@ -11,15 +11,40 @@ public class ScrimmageAutoBlue extends LinearOpMode {
     public DcMotor motorFL;
     public DcMotor motorBR;
     public DcMotor motorBL;
+    double eFL;
+    double eFR;
+    double eBL;
+    double eBR;
+    double changeFL;
+    double changeFR;
+    double changeBL;
+    double changeBR;
     @Override
     public void runOpMode() throws InterruptedException {
-        motorFL = hardwareMap.dcMotor.get("motorFL");
-        motorFR = hardwareMap.dcMotor.get("motorFR");
-        motorBL = hardwareMap.dcMotor.get("motorBL");
-        motorBR = hardwareMap.dcMotor.get("motorBR");
+        motorFL = hardwareMap.dcMotor.get("motorFR");
+        motorFR = hardwareMap.dcMotor.get("motorFL");
+        motorBL = hardwareMap.dcMotor.get("motorBR");
+        motorBR = hardwareMap.dcMotor.get("motorBL");
+
 
         waitForStart();
 
 
+    }
+
+    public void moveForward(double speed, double distance) {
+        reset();
+        
+    }
+
+    public void reset() {
+        eFL = 0;
+        eFR = 0;
+        eBL = 0;
+        eBR = 0;
+        changeFL = 0;
+        changeFR = 0;
+        changeBR = 0;
+        changeBL = 0;
     }
 }
