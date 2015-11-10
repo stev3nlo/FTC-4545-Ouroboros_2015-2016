0222e35173351cd75b70e3dca35d4d188e1318d9
nat4545 <natalie.root70@k12.leanderisd.org>
2015-11-10 17:00:17 -0600

    New Dry Run files

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/AutoDryRun.java
@@ -0,0 +1,15 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.Code;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by natalieroot on 11/10/15.
+ */
+public class AutoDryRun extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/TeleOpDryRun.java
@@ -0,0 +1,20 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.Code;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+/**
+ * Created by natalieroot on 11/10/15.
+ */
+public class TeleOpDryRun extends OpMode {
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
