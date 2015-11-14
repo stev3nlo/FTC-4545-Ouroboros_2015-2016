3aa695402c526d4ce5fa0f58f380893ff46eec2f
Kunal Gupta <kunalgupta@Kunals-MacBook-Pro.local>
2015-11-14 12:34:50 -0600

    ColorSensor Test

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/RGBtest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/RGBtest.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.view.View;
 import com.qualcomm.ftccommon.DbgLog;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 //import com.qualcomm.ftcRobotcontroller.R;
 //import com.qualcomm.Robotcore.eventloop.opmode.LinearOpMode;
 //import com.qualcomm.Robotcore.hardware.ColorSensor;
@@ -17,6 +18,8 @@ import com.qualcomm.ftccommon.DbgLog;
  * Created by Vandegrift InvenTeam on 11/3/2015.
  */
 public class RGBtest extends OpMode{
+
+    ColorSensor colorSensor;
 
 
     @Override
