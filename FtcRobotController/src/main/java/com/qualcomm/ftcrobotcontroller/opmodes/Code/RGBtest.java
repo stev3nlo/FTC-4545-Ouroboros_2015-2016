6401f6b778e7ef5187cfd406cc736d162a8f0d52
Team 4545 <provincerice@gmail.com>
2015-11-17 17:39:32 -0600

    Fixed RGBtest, Added to Register

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/RGBtest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/RGBtest.java
@@ -1,6 +1,7 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.Code;
 
 import com.qualcomm.ftcrobotcontroller.R;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import android.app.Activity;
@@ -21,70 +22,77 @@ import com.qualcomm.robotcore.hardware.LED;
 /**
  * Created by Vandegrift InvenTeam on 11/3/2015.
  */
-public class RGBtest extends OpMode{
+public class RGBtest extends LinearOpMode{
 
     ColorSensor colorSensor;
     DeviceInterfaceModule cdim;
     static final int LED_CHANNEL = 5;
 
-    boolean bEnabled = false;
-    boolean bPrevState = false;
-    boolean bCurrState = false;
-
-    float hsvValues[] = {0F,0F,0F};
-    final float values[] = hsvValues;
-
-    final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(R.id.RelativeLayout);
-
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
+
+        boolean bEnabled = false;
+        boolean bPrevState = false;
+        boolean bCurrState = false;
+
+        float hsvValues[] = {0F, 0F, 0F};
+        final float values[] = hsvValues;
+
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(R.id.RelativeLayout);
+
         cdim = hardwareMap.deviceInterfaceModule.get("dim");
 
         cdim.setDigitalChannelMode(LED_CHANNEL, DigitalChannelController.Mode.OUTPUT);
 
         colorSensor = hardwareMap.colorSensor.get("lady");
 
-        cdim.setDigitalChannelState(LED_CHANNEL,bEnabled);
+        cdim.setDigitalChannelState(LED_CHANNEL, bEnabled);
+
+        while (opModeIsActive()) {
+            bCurrState = gamepad1.x || gamepad2.x;
+
+            if (bCurrState == true && bCurrState != bPrevState) {
+
+                DbgLog.msg("MY_DEBUG - x button was pressed! ");
+
+                bPrevState = bCurrState;
+
+                bEnabled = false;
+
+                cdim.setDigitalChannelState(LED_CHANNEL, bEnabled);
+            } else if (bCurrState == false && bCurrState != bPrevState) {
+
+                DbgLog.msg("MY_DEBUG - x button was released! ");
+
+                bPrevState = bCurrState;
+
+                bEnabled = false;
+
+                cdim.setDigitalChannelState(LED_CHANNEL, bEnabled);
+            }
+
+            Color.RGBToHSV((colorSensor.red() * 255) / 800, (colorSensor.green() * 255) / 800, (colorSensor.blue() * 255) / 800, hsvValues);
+
+            telemetry.addData("Clear", colorSensor.alpha());
+            telemetry.addData("Red  ", colorSensor.red());
+            telemetry.addData("Green", colorSensor.green());
+            telemetry.addData("Blue ", colorSensor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+
+            relativeLayout.post(new Runnable() {
+                    public void run() {
+                        relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                    }
+            });
+
+            waitOneFullHardwareCycle();
+        }
     }
 
-    @Override
-    public void loop() {
-
-        bCurrState = gamepad1.x || gamepad2.x;
-
-        if (bCurrState == true && bCurrState != bPrevState) {
-
-            DbgLog.msg("MY_DEBUG - x button was pressed! ");
-
-            bPrevState = bCurrState;
-
-            bEnabled = false;
-
-            cdim.setDigitalChannelState(LED_CHANNEL, bEnabled);
-        }
-        else if (bCurrState == false && bCurrState != bPrevState) {
-
-            DbgLog.msg("MY_DEBUG - x button was released! ");
-
-            bPrevState = bCurrState;
-
-            bEnabled = false;
-
-            cdim.setDigitalChannelState(LED_CHANNEL,bEnabled);
-        }
-
-        Color.RGBToHSV((colorSensor.red() * 255) / 800, (colorSensor.green() * 255) / 800, (colorSensor.blue() * 255) / 800, hsvValues);
-
-        telemetry.addData("Clear", colorSensor.alpha());
-        telemetry.addData("Red  ", colorSensor.red());
-        telemetry.addData("Green", colorSensor.green());
-        telemetry.addData("Blue ", colorSensor.blue());
-        telemetry.addData("Hue", hsvValues[0]);
-
-        relativeLayout.post(new Runnable() {
-            public void run() {
-                relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
-            }
-        });
+    public void waitOneFullHardwareCycle() throws InterruptedException {
+        this.waitForNextHardwareCycle();
+        Thread.sleep(1L);
+        this.waitForNextHardwareCycle();
     }
 }
+
