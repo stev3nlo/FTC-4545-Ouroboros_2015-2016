57d8fe16efc65ca8844acafaa8fe5b0c1996d2b5
sonofesh <r3spatel@gmail.com>
2015-12-15 17:04:24 -0600

    New Tele-Op!!!!!!!!! Added normal method

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/Teleop3.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/Teleop3.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.*;
 import com.qualcomm.robotcore.hardware.configuration.ServoConfiguration;
 
+
 /**
  * Created by sopa on 12/14/15.
  */
@@ -40,9 +41,61 @@ public class Teleop3 extends OpMode{
     public void loop() {
 
     }
-    public void moveBase(
-            if(gamepad1.right_stick > .1){
-                int i = 0;
+    public void Normal(){
+        //base
+        if(Math.abs(gamepad1.right_stick_y) > .1){
+            motorFR.setPower(gamepad1.right_stick_y * -1);
+            motorBR.setPower(gamepad1.right_stick_y * -1);
         }
-    )
+        else {
+            motorFR.setPower(0);
+            motorBR.setPower(0);
+        }
+        //servo for flipping switches
+        if (gamepad1.left_bumper) {
+            switchL.setPosition(1);
+        }
+        else {
+            switchL.setPosition(.5);
+        }
+        if (gamepad1.right_bumper) {
+            switchR.setPosition(0);
+        }
+        else {
+            switchR.setPosition(.5);
+        }
+        if(Math.abs(gamepad1.left_stick_y) > .1){
+            motorFL.setPower(gamepad1.left_stick_y);
+            motorBL.setPower(gamepad1.left_stick_y);
+        }
+        else {
+            motorFL.setPower(0);
+            motorBL.setPower(0);
+        }
+        //manipulator
+        if(Math.abs(gamepad2.left_stick_y) > .1){
+            motorSpinner.setPower(gamepad2.left_stick_y);
+        }
+        else {
+            motorSpinner.setPower(0);
+        }
+        //lift
+        if(Math.abs(gamepad2.right_stick_y) > .1){
+            motorHangL.setPower(gamepad2.right_stick_y);
+            motorHangR.setPower(gamepad2.right_stick_y * -1);
+        }
+        else {
+            motorHangL.setPower(0);
+            motorHangR.setPower(0);
+        }
+        //servo climber switch
+        if(gamepad2.x){
+            climber.setPosition(1);
+        }
+        else
+        {
+            climber.setPosition(0);
+        }
+    }
 }
+
