2c472980122c8e65624919ef0ca85511ed131b2b
sonofesh <r3spatel@gmail.com>
2016-01-02 15:07:35 -0600

    added test for manipulator and lift
    test program for new robot

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/Test.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/Test.java
@@ -11,34 +11,61 @@ public class Test extends OpMode{
     public DcMotor motorFL;
     public DcMotor motorBR;
     public DcMotor motorBL;
+    public DcMotor manipulator;
+    public DcMotor liftR;
+    public DcMotor liftL;
+
     @Override
     public void init() {
         motorFL = hardwareMap.dcMotor.get("motorFL");
         motorFR = hardwareMap.dcMotor.get("motorFR");
         motorBL = hardwareMap.dcMotor.get("motorBL");
         motorBR = hardwareMap.dcMotor.get("motorBR");
+        manipulator = hardwareMap.dcMotor.get("manipulator");
+        liftR = hardwareMap.dcMotor.get("liftR");
+        liftL = hardwareMap.dcMotor.get("liftL");
     }
 
     @Override
     public void loop() {
         //tests wheels
         if(Math.abs(gamepad1.right_stick_y) > .05){
-            motorFR.setPower(gamepad1.right_stick_y * -1);
-            motorBR.setPower(gamepad1.right_stick_y * -1);
+            motorFR.setPower(gamepad1.right_stick_y);
+            motorBR.setPower(gamepad1.right_stick_y);
         }
         else{
             motorFR.setPower(0);
             motorBR.setPower(0);
         }
         if(Math.abs(gamepad1.left_stick_y) > .05){
-            motorFL.setPower(gamepad1.left_stick_y);
-            motorBL.setPower(gamepad1.left_stick_y);
+            motorFL.setPower(gamepad1.left_stick_y * -1);
+            motorBL.setPower(gamepad1.left_stick_y * -1);
         }
         else{
             motorFL.setPower(0);
             motorBL.setPower(0);
         }
-
+        //REMEMBER TO FLIP!!!!
+        //test manipulator
+        if(Math.abs(gamepad1.right_trigger) > .1)
+        {
+            if(gamepad1.a){
+                manipulator.setPower(gamepad1.right_trigger * -1);
+            }
+            else{
+                manipulator.setPower(gamepad1.right_trigger);
+            }
+        }
+        //test lift
+        if(Math.abs(gamepad1.left_trigger) > .1){
+            if(gamepad1.b){
+                liftR.setPower(gamepad1.left_trigger * -1);
+                liftL.setPower(gamepad1.left_trigger * -1);
+            }
+            else{
+                liftR.setPower(gamepad1.left_trigger);
+                liftL.setPower(gamepad1.left_trigger);
+            }
+        }
     }
-
 }
