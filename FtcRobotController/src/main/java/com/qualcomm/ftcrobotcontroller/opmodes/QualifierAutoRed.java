23b9d113bee32218385aafa924362614a4dd81b8
Team 4545 <provincerice@gmail.com>
2016-01-07 16:53:25 -0600

    New autonomous program

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/QualifierAutoRed.java
@@ -0,0 +1,13 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Vandegrift InvenTeam on 1/7/2016.
+ */
+public class QualifierAutoRed extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
