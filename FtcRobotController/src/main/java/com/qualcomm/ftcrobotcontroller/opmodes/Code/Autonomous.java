7926d07be721c5da46a7ad38f89788676f92cadc
stev3nlo <stev3nlo17@gmail.com>
2016-01-09 14:47:35 -0600

    attempted to fix qualifierautored

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/Autonomous.java
@@ -0,0 +1,20 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.Code;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Steven on 1/9/2016.
+ */
+public class Autonomous extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor motorFR;
+        DcMotor motorFL;
+        DcMotor motorBR;
+        DcMotor motorBL;
+        DcMotor manipulator;
+        Servo climber;
+    }
+}
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/QualifierAutoRed.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Code/QualifierAutoRed.java
@@ -3,6 +3,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes.Code;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.RobotLog;
 
 /**
  * Created by Vandegrift InvenTeam on 1/7/2016.
@@ -14,13 +15,20 @@ public class QualifierAutoRed extends LinearOpMode {
     DcMotor motorBL;
     DcMotor manipulator;
     Servo climber;
-    int eBL;
-    int eBR;
-    int eAvg;
+    int BR = 0;
+    int BL = 0;
+    int curr = 0;
+    int avg = 0;
+    int start = 0;
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        waitOneFullHardwareCycle();
+    public void runOpMode() {
+        try {
+            waitOneFullHardwareCycle();
+        } catch (InterruptedException e) {
+            RobotLog.e(e.getMessage());
+        }
+
         motorFR = hardwareMap.dcMotor.get("motorFR");
         motorFL = hardwareMap.dcMotor.get("motorFL");
         motorBR = hardwareMap.dcMotor.get("motorBR");
@@ -29,7 +37,11 @@ public class QualifierAutoRed extends LinearOpMode {
         climber = hardwareMap.servo.get("climber");
         climber.setPosition(1);
 
-        waitForStart();
+        try {
+            waitForStart();
+        } catch (InterruptedException e) {
+            RobotLog.e(e.getMessage());
+        }
 
         moveForward(1, 5000);
         moveBackwards(1, 5000);
@@ -39,38 +51,11 @@ public class QualifierAutoRed extends LinearOpMode {
         turnRight(0.5, 3000);
     }
 
-    public void reset() {
-        motorFR.setPower(0);
-        motorFL.setPower(0);
-        motorBR.setPower(0);
-        motorBL.setPower(0);
-        eBL = Math.abs(motorBL.getCurrentPosition());
-        eBR = Math.abs(motorBR.getCurrentPosition());
-        update();
-        manipulator.setPower(0);
-    }
-
-    public void update() {
-        eBR = Math.abs(motorBR.getCurrentPosition());
-        eBL = Math.abs(motorBL.getCurrentPosition());
-        eAvg = (Math.abs(eBR) + Math.abs(eBL)) / 2;
-    }
     public void moveForward(double speed, int distance) {
-        int start = eAvg;
-        while (eAvg < start + distance) {
-            showData();
-            motorFR.setPower(speed);
-            motorFL.setPower(-speed);
-            motorBR.setPower(speed);
-            motorBL.setPower(-speed);
-            update();
-        }
-        reset();
-    }
-
-    public void moveBackwards(double speed, int distance) {
-        int start = eAvg;
-        while (eAvg < start + distance) {
+        update();
+        start = curr;
+        curr = 0;
+        while ((curr) < distance) {
             showData();
             motorFR.setPower(-speed);
             motorFL.setPower(speed);
@@ -81,29 +66,42 @@ public class QualifierAutoRed extends LinearOpMode {
         reset();
     }
 
-    public void forwardWithMani(double speed, int distance) {
-        int start = eAvg;
-        while (eAvg < start + distance) {
+    public void moveBackwards(double speed, int distance) {
+        update();
+        start = curr;
+        curr = 0;
+        while ((curr) < distance) {
             showData();
             motorFR.setPower(speed);
             motorFL.setPower(-speed);
             motorBR.setPower(speed);
             motorBL.setPower(-speed);
+            update();
+        }
+        reset();
+    }
+
+    public void forwardWithMani(double speed, int distance) {
+        update();
+        start = curr;
+        curr = 0;
+        while ((curr) < distance) {
+            showData();
+            motorFR.setPower(-speed);
+            motorFL.setPower(speed);
+            motorBR.setPower(-speed);
+            motorBL.setPower(speed);
             manipulator.setPower(-1);
             update();
         }
         reset();
     }
 
-    public void dropClimbers() throws InterruptedException {
-        climber.setPosition(0);
-        wait(500);
-        climber.setPosition(.5);
-    }
-
     public void turnRight(double speed, int amount) {
-        int start = eAvg;
-        while (eAvg < start + amount) {
+        update();
+        start = curr;
+        curr = 0;
+        while ((curr) < amount) {
             showData();
             motorFR.setPower(speed);
             motorFL.setPower(speed);
@@ -115,8 +113,10 @@ public class QualifierAutoRed extends LinearOpMode {
     }
 
     public void turnLeft(double speed, int amount) {
-        int start = eAvg;
-        while (eAvg < start + amount) {
+        update();
+        start = curr;
+        curr = 0;
+        while ((curr) < amount) {
             showData();
             motorFR.setPower(-speed);
             motorFL.setPower(-speed);
@@ -127,9 +127,30 @@ public class QualifierAutoRed extends LinearOpMode {
         reset();
     }
 
+    public void dropClimbers() {
+        climber.setPosition(0);
+        //wait(500);
+        climber.setPosition(.5);
+    }
+
+    public void reset() {
+        motorFR.setPower(0);
+        motorFL.setPower(0);
+        motorBR.setPower(0);
+        motorBL.setPower(0);
+        start = curr;
+        update();
+        manipulator.setPower(0);
+    }
+
+    public void update() {
+        BR = Math.abs(motorBR.getCurrentPosition());
+        BL = Math.abs(motorBL.getCurrentPosition());
+        avg = (BR + BL) / 2;
+        curr = avg - start;
+    }
+
     public void showData() {
-        telemetry.addData("eBR", eBR);
-        telemetry.addData("eBL", eBL);
-        telemetry.addData("eAvg", eAvg);
+        telemetry.addData("current position", curr);
     }
 }
